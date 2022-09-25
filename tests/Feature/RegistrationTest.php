0fa05f844c699fbcbb52217a2758eae8a82213d9
DherismaHanindita <dherismadedek@gmail.com>
2022-09-26 00:33:52 +0700

    whitebox

--- tests/Feature/RegistrationTest.php
+++ tests/Feature/RegistrationTest.php
@@ -6,15 +6,15 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class RegistrationTes extends TestCase
-{
-    /**
+ {
+    /** 
      * A basic test example.
      *
      * @return void
      */
     public function test_registrationtest()
     {
-        $response = $this->get('/');
+        $response = $this->get('/');  
 
         $response->assertStatus(200);
     }
